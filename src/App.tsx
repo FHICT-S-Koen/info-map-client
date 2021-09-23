b8a618e487c64ab3346bb8f39fc28153eba1580a
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-09-24 01:46:28 +0200

    Added auth0 authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import Login from './Login';
+import Logout from './Logout';
+
 export default function App() {
-  return (
-    <div>
- 
-    </div>
-  );
+  const {isAuthenticated} = useAuth0()
+
+  if (isAuthenticated) return (
+    <>
+      <div>Your logged in!</div>
+      <Logout />
+    </>)
+  else return (
+    <>
+      <div>Your logged out!</div>
+      <Login />
+    </>)
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain="dev-lq75abqz.eu.auth0.com"
+    clientId="kDmXepiyYs05lIAzUzlzxuJgoeqp37L7"
+    redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
