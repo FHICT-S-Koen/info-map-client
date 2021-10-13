c2e3e9722adc9ae059a28ba90b2b59a77c523520
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-10-14 01:15:24 +0200

    Added tailwind + heroicons and re-styled the navbar with tailwind

--- src/auth/AuthButton.tsx
+++ src/auth/AuthButton.tsx
@@ -1,11 +1,10 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
-import '../styles/NavBar.css'
 
 export default function AuthButton() {
   const { isAuthenticated, isLoading } = useAuth0()
 
-  if (isLoading) return <div className="Auth">loading ...</div>
+  if (isLoading) return <div className="text-green-400 p-2">loading ...</div>
   else return isAuthenticated ? <LogoutButton /> : <LoginButton />
 };
